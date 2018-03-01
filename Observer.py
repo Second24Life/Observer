a8140199f6697323ae902265b287659e38aa1e40
Max Zerubitskiy <maka123456789@mail.ru>
2018-03-01 21:39:53 +0700

    Initial commit

---
+++ Observer.py
@@ -0,0 +1,50 @@
+from abc import ABCMeta, abstractmethod
+
+class Observer():
+	__metaclass__=ABCMeta
+
+	@abstractmethod
+	def update(self, message):
+		pass
+
+class Observable():
+	__metaclass__=ABCMeta
+
+	def __init__(self):
+		self.observers = []
+
+	def register(self, observer):
+		self.observers.append(observer)
+
+	def detach(self, observer):
+		if observer in self.observers:
+			self.observers.remove(observer)
+
+	def notify(self, message):
+		for observer in self.observers:
+			observer.update(message)
+
+class Newspaper(Observable):
+
+	def __init__(self, name, yearOfCreate):
+		self.name = name
+		self.yearOfCreate = yearOfCreate
+
+	def add_news(self, news):
+		self.notify(news)
+
+class Citizen(Observer):
+
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+
+	def update(self, message):
+		print('{} learned the news: {}'.format(self.name, message))
+
+if __name__ == '__name__':
+	newspaper = Newspaper('City', 1980)
+	newspaper.register(Citizen('Max', 20))
+	newspaper.register(Citizen('Igor', 20))
+
+	newspaper.add_news('Good news came to our city ...')
