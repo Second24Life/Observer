bf5ff2460203d23a620043a1bfef6a7a4200f649
Max Zerubitskiy <maka123456789@mail.ru>
2018-03-02 07:55:39 +0700

    pattern create

--- Observer.py
+++ Observer.py
@@ -1,5 +1,8 @@
 from abc import ABCMeta, abstractmethod
 
+# Абстрактный класс для наблюдателя
+# имеет метод update, который необходимо обязательно переопределить в наследуемых классах
+# метод update выполняет какое-либо действие после того как наблюдаемый объект сообщить об событии
 class Observer():
 	__metaclass__=ABCMeta
 
@@ -7,11 +10,13 @@ class Observer():
 	def update(self, message):
 		pass
 
+# Абстрактный класс для наблюдаемого объекта
+# метод register добавляет новых наблюдателей
+# метод detach отцепляет наблюдателя от наблюдаемого объекта
+# метод notify сообщает наблюдателям о новом событии
 class Observable():
 	__metaclass__=ABCMeta
-
-	def __init__(self):
-		self.observers = []
+	observers = []
 
 	def register(self, observer):
 		self.observers.append(observer)
@@ -24,6 +29,8 @@ class Observable():
 		for observer in self.observers:
 			observer.update(message)
 
+# Класс газеты и немного минимум информации
+# метод add_news вызывает метод notify из абстрактного класса, чтобы сообщить о новости 
 class Newspaper(Observable):
 
 	def __init__(self, name, yearOfCreate):
@@ -33,6 +40,8 @@ class Newspaper(Observable):
 	def add_news(self, news):
 		self.notify(news)
 
+# Класс горожанина и немного информации о нем
+# метод update переопределен
 class Citizen(Observer):
 
 	def __init__(self, name, age):
@@ -42,15 +51,20 @@ class Citizen(Observer):
 	def update(self, message):
 		print('{} learned the news: {}'.format(self.name, message))
 
-if __name__ == '__name__':
-	newspaper = Newspaper('City', 1980)
-	maksim = Citizen('Max', 20)
-	igor = Citizen('Igor', 20)
-	newspaper.register(maksim)
-	newspaper.register(igor)
+# Создаем объект класса Newspaper
+newspaper = Newspaper('City', 1980)
+# Создаем объекты класса Citizen
+maksim = Citizen('Max', 20)
+igor = Citizen('Igor', 20)
+# Подписываем созданных горожан на газету
+newspaper.register(maksim)
+newspaper.register(igor)
 
-	newspaper.add_news('Good news came to our city ...')
+# Газета добавляет новую новость и сообщает о ней подписчикам
+newspaper.add_news('Good news came to our city ...')
 
-	newspaper.detach(igor)
+# Один из горожан решил отписаться от газеты
+newspaper.detach(igor)
 
-	newspaper.add_news('We lost the Igor')
+# Газета добавляет новость о отписке своего подпичсика
+newspaper.add_news('We lost the Igor')
